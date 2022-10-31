302c1102f9f4765f3773e065ef0f9ad03576ad82
Александр Бондарай <bondoray@mail.ru>
2022-10-31 22:20:57 +0800

    fix: type error

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as number
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
@@ -13,6 +13,7 @@ root.render(
   </React.StrictMode>
 );
 
+//test comment 1
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
